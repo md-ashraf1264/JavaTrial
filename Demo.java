3bebc18b15b920ef4a64ec935c396682c0f9baac
Md Ashraf <ashrafsheikhbgp82@gmail.com>
2023-08-06 18:51:36 +0530

    Ashraf.java modified add m2()

--- Demo.java
+++ Demo.java
@@ -4,4 +4,8 @@ public static void main(String args[])
 {
  System.out.println("First Java Program.....");
 }
+public static void main(String args[])
+{
+ System.out.println("First Java Program.....");
+}
 }
